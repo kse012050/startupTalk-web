20f224220942f0f120f3ffd3ca90715b063d496d
김성은 <kse012050@naver.com>
2023-03-23 15:34:27 +0900

    카테고리 메인 페이지 완료

--- src/components/CategoryType.jsx
+++ src/components/CategoryType.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function CategoryType({info , title}) {
+    const [isDrop , idSetDrop] = useState(false);
+    
     return (
         <div className='categoryType'>
-            {title && <h3>{info.title}</h3>}
-            <ul>
-                {info.typeList.map((c)=>
-                    <li key={c.title}>
-                        <Link to={`/category/show/${info.type}`}>{c.title}
-                            {!!c.count && <mark>{c.count}</mark>}
-                        </Link>
-                    </li>
-                )}
-            </ul>
+            {title && <h3 onClick={()=>idSetDrop(!isDrop)} className={isDrop ? 'active' : null}>{info.title}</h3>}
+            {isDrop &&
+                <ul>
+                    {info.typeList.map((c)=>
+                        <li key={c.title}>
+                            <Link to={`/category/show/${info.type}`}>{c.title}
+                                {!!c.count && <mark>{c.count}</mark>}
+                            </Link>
+                        </li>
+                    )}
+                </ul>
+            }
         </div>
     );
 }
