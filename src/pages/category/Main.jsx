e3c27cd942fc43fe46e89635983a2e0bf67332cc
김성은 <kse012050@naver.com>
2023-05-15 02:45:59 +0900

    검색 css 완성

--- src/pages/category/Main.jsx
+++ src/pages/category/Main.jsx
@@ -34,15 +34,26 @@ export default function Main() {
             </h2>
             <Search body={[body , setBody]}/>
             {!params.search && categoryData?.map((data)=><CategoryType key={data.parent_category} info={data.categorys} title={data.parent_category}/>)}
-            {(!!params.search && searchData?.data.total_count) && (
+            {(!!params.search) && (
                 <>
-                    <div className='searchTotal'>
-                        <p>검색결과 총<mark>{searchData.data.total_count}개</mark></p>
-                        <DropBox type='small'  first={[firstOrder , setFirstOrder]} list={order} body={[body , setBody]}/>
-                    </div>
-                    <Horizontal4>
-                        {searchData.list.map((data)=><li key={data.brand_id}><Item img={data.brand_main_store_file} info={{title : data.brand_name , shop : data.store_count , cost : data.start_up_money / 1000 , taik : data.chat_count}} support={data.promotion_yn} bookMark={data.interest_yn} id={data.brand_id}/></li>)}
-                    </Horizontal4>
+                    {searchData?.data.total_count !== '0' &&
+                        (
+                            <>
+                                <div className='searchTotal'>
+                                    <p>검색결과 총<mark>{searchData?.data.total_count}개</mark></p>
+                                    <DropBox type='small'  first={[firstOrder , setFirstOrder]} list={order} body={[body , setBody]}/>
+                                </div>
+                                <Horizontal4>
+                                    {searchData?.list.map((data)=><li key={data.brand_id}><Item img={data.brand_main_store_file} info={{title : data.brand_name , shop : data.store_count , cost : data.start_up_money / 1000 , taik : data.chat_count}} support={data.promotion_yn} bookMark={data.interest_yn} id={data.brand_id}/></li>)}
+                                </Horizontal4>
+                            </>
+                        )
+                    }
+                    {searchData?.data.total_count === '0' && 
+                        <div className='searchFail'>
+                            검색된 아이템, 브랜드가 없습니다.
+                        </div>
+                    }
                 </>
                 )
             }
